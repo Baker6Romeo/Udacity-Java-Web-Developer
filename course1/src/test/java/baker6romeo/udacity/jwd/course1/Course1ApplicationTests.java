703d5e2f70cea89b1e46093e7fa05b8da53a2e39
Shaw <Jonathan.A.Shaw@gd-ms.us>
2021-07-24 09:36:07 -0400

    Pushing first project to move from work to dev laptop.

---
+++ course1/src/test/java/baker6romeo/udacity/jwd/course1/Course1ApplicationTests.java
@@ -0,0 +1,13 @@
+package baker6romeo.udacity.jwd.course1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Course1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
